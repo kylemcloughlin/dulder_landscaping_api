adbfe7d5e4941489801d71d102acf2eb3cf9055e
kylemcloughlin <kylemcloughlin1000@hotmail.ca>
2021-08-25 10:19:05 -0400

    adding customers changes

--- app/controllers/customer_controller.rb
+++ app/controllers/customer_controller.rb
@@ -3,10 +3,25 @@ Stripe.api_key = "sk_test_51JPZAlKUlzH2P3ixCMvTZMHvmZLYvCn9j2XldVoVf4qeBgCW1bhgo
 
 class CustomerController < ApplicationController
     def create
-   
+    shipping = params['shipping']
+    billing = params['billing']
+    cart = params['cart']
+    total_cost = 0
+    sold = []
+
+  # byebug
+    cart.each do |item|
+      helper = item['value'].to_i * item['price'].to_i
+      total_cost =  total_cost + helper
+      sold  << "#{item['value']} #{item['name']} at #{item['price']}"
+    end
+    hst = total_cost * 0.13
+    hst = hst.floor
+    # byebug
   payment_intent = Stripe::PaymentIntent.create({
-  amount: 2000,
+  amount: total_cost + hst,
   currency: "cad",
+  description: "discription of sold items: #{sold}", 
   payment_method_types: ["card"],
 })
 
@@ -14,7 +29,6 @@ confirm = Stripe::PaymentIntent.confirm(
   payment_intent.id,
   { payment_method: "pm_card_visa" },
 )
-# byebug
       if confirm.status === 'succeeded'
      
         render json: { payment_status: confirm.status, confirm: confirm }
