a469364bbdbfdb2346d9f2291e434dbc2114716a
kylemcloughlin <kylemcloughlin1000@hotmail.ca>
2021-09-27 13:53:27 -0400

    adding cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://localhost:3000', 'https://shrouded-crag-98843.herokuapp.com', 'http://shrouded-crag-98843.herokuapp.com', 'https://master.dml2euvkz2dhi.amplifyapp.com/landscaping-supplies', 'http://master.dml2euvkz2dhi.amplifyapp.com/landscaping-supplies']
+    origins ['http://localhost:3000', 'https://shrouded-crag-98843.herokuapp.com', 'http://shrouded-crag-98843.herokuapp.com', 'https://master.dml2euvkz2dhi.amplifyapp.com', 'http://master.dml2euvkz2dhi.amplifyapp.com']
     resource '*',
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
